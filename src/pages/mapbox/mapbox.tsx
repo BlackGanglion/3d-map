d5943fbcc67248d55706fab3e2d43e0c38f7d0a3
BlackGanglion <BlackGanglion@blackganglion.com>
2025-03-15 15:47:24 +0800

    feat: add mapbox 3D

--- src/pages/mapbox/mapbox.tsx
+++ src/pages/mapbox/mapbox.tsx
@@ -1,69 +1,219 @@
 import { useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
+import * as turf from '@turf/turf';
+
+import { token } from '../../../config';
+import { data } from '../../../data/female-stage-2';
+import { flyInAndRotate, createGeoJSONCircle, animatePath } from './utils';
+
 import 'mapbox-gl/dist/mapbox-gl.css';
 import styles from './mapbox.less';
-import { token } from '../../../config';
 
 mapboxgl.accessToken = token;
+
+const initMap = async () => {
+  const map = new mapboxgl.Map({
+    // style: 'mapbox://styles/blackganglion/cm4ps80vc006101su8ofl1z6q', // style URL
+    style: 'mapbox://styles/mapbox/satellite-streets-v12',
+    container: 'map',
+    zoom: 1.9466794621990684,
+    center: { lng: 12.563530000000014, lat: 58.27372323078674 },
+    pitch: 70,
+    bearing: 0,
+    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+    // style: 'mapbox://styles/mapbox/standard'
+  });
+
+  const addPathSourceAndLayer = (trackGeojson: any) => {
+    // Add a line feature and layer. This feature will get updated as we progress the animation
+    map.addSource("line", {
+      type: "geojson",
+      // Line metrics is required to use the 'line-progress' property
+      lineMetrics: true,
+      data: trackGeojson,
+    });
+    map.addLayer({
+      id: "line-layer",
+      type: "line",
+      source: "line",
+      paint: {
+        "line-color": "rgba(0,0,0,0)",
+        "line-width": 9,
+        "line-opacity": 0.8,
+      },
+      layout: {
+        "line-cap": "round",
+        "line-join": "round",
+      },
+    });
+
+    map.addSource("start-pin-base", {
+      type: "geojson",
+      data: createGeoJSONCircle(trackGeojson.geometry.coordinates[0], 0.04)
+    });
+
+    map.addSource("start-pin-top", {
+      type: "geojson",
+      data: createGeoJSONCircle(trackGeojson.geometry.coordinates[0], 0.25)
+    });
+
+    map.addSource("end-pin-base", {
+      type: "geojson",
+      data: createGeoJSONCircle(trackGeojson.geometry.coordinates.slice(-1)[0], 0.04)
+    });
+
+    map.addSource("end-pin-top", {
+      type: "geojson",
+      data: createGeoJSONCircle(trackGeojson.geometry.coordinates.slice(-1)[0], 0.25)
+    });
+
+    map.addLayer({
+      id: "start-fill-pin-base",
+      type: "fill-extrusion",
+      source: "start-pin-base",
+      paint: {
+        'fill-extrusion-color': '#0bfc03',
+        'fill-extrusion-height': 1000
+      }
+    });
+    map.addLayer({
+      id: "start-fill-pin-top",
+      type: "fill-extrusion",
+      source: "start-pin-top",
+      paint: {
+        'fill-extrusion-color': '#0bfc03',
+        'fill-extrusion-base': 1000,
+        'fill-extrusion-height': 1200
+      }
+    });
+
+    map.addLayer({
+      id: "end-fill-pin-base",
+      type: "fill-extrusion",
+      source: "end-pin-base",
+      paint: {
+        'fill-extrusion-color': '#eb1c1c',
+        'fill-extrusion-height': 1000
+      }
+    });
+    map.addLayer({
+      id: "end-fill-pin-top",
+      type: "fill-extrusion",
+      source: "end-pin-top",
+      paint: {
+        'fill-extrusion-color': '#eb1c1c',
+        'fill-extrusion-base': 1000,
+        'fill-extrusion-height': 1200
+      }
+    });
+  };
+
+  const playAnimations = async (trackGeojson: any) => {
+    return new Promise(async (resolve) => {
+      // add a geojson source and layer for the linestring to the map
+      addPathSourceAndLayer(trackGeojson);
+
+      // get the start of the linestring, to be used for animating a zoom-in from high altitude
+      const targetLngLat = {
+        lng: trackGeojson.geometry.coordinates[0][0],
+        lat: trackGeojson.geometry.coordinates[0][1],
+      };
+
+      // animate zooming in to the start point, get the final bearing and altitude for use in the next animation
+      const { bearing, altitude } = await flyInAndRotate({
+        map,
+        targetLngLat,
+        duration: 7000,
+        startAltitude: 3000000,
+        endAltitude: 12000,
+        startBearing: 0,
+        endBearing: -20,
+        startPitch: 40,
+        endPitch: 50,
+      });
+
+      // follow the path while slowly rotating the camera, passing in the camera bearing and altitude from the previous animation
+      await animatePath({
+        map,
+        duration: 60000,
+        path: trackGeojson,
+        startBearing: bearing,
+        startAltitude: altitude,
+        pitch: 50
+      });
+
+      // get the bounds of the linestring, use fitBounds() to animate to a final view
+      const bounds = turf.bbox(trackGeojson) as any;
+      map.fitBounds(bounds, {
+        duration: 3000,
+        pitch: 30,
+        bearing: 0,
+        padding: 120,
+      });
+
+      setTimeout(() => {
+        resolve(null);
+      }, 10000)
+    });
+  }
+
+  const add3D = () => {
+    // add map 3d terrain and sky layer and fog
+    // Add some fog in the background
+    map.setFog({
+      range: [0.5, 10],
+      color: "white",
+      "horizon-blend": 0.2,
+    });
+
+    // Add a sky layer over the horizon
+    map.addLayer({
+      id: "sky",
+      type: "sky",
+      paint: {
+        "sky-type": "atmosphere",
+        "sky-atmosphere-color": "rgba(85, 151, 210, 0.5)",
+      },
+    });
+
+    map.addSource('mapbox-dem', {
+      'type': 'raster-dem',
+      'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+      'tileSize': 512,
+      'maxzoom': 14
+    });
+    // add the DEM source as a terrain layer with exaggerated height
+    map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 2 });
+  };
+
+  map.on('style.load', async () => {
+    // add 3d, sky and fog
+    add3D();
+
+    // wait until the map settles
+    await map.once('idle');
+
+    // fetch the geojson for the linestring to be animated
+    // kick off the animations
+    await playAnimations(data);
+  });
+
+  function printMapState() {
+    /*
+    console.log("Zoom: " + map.getZoom());
+    console.log("Center: " + JSON.stringify(map.getCenter()));
+    console.log("Pitch: " + map.getPitch());
+    console.log("Bearing: " + map.getBearing());
+    */
+  }
+
+  map.on('moveend', printMapState);
+}
 
 const Mapbox = () => {
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      // style: 'mapbox://styles/blackganglion/cm4ps80vc006101su8ofl1z6q', // style URL
-      style: 'mapbox://styles/mapbox/satellite-streets-v12',
-
-      container: 'map',
-      zoom: 12.13677902205691,
-      center: [11.05562895964988, 47.419526884419895],
-      pitch: 44.62271649269611,
-      bearing: -151.49447725668517,
-      // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-      // style: 'mapbox://styles/mapbox/standard'
-    });
-
-    map.on('style.load', () => {
-      map.addSource('mapbox-dem', {
-        'type': 'raster-dem',
-        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-        'tileSize': 512,
-        'maxzoom': 14
-      });
-      // add the DEM source as a terrain layer with exaggerated height
-      map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 2 });
-
-      /*
-      map.addSource("trace", {
-        type: "geojson",
-        lineMetrics: true,
-        data: (data as any).data,
-      });
-
-      map.addLayer({
-        type: 'line',
-        source: 'trace',
-        id: 'line',
-        paint: {
-          'line-color': 'blue',
-          'line-width': 2,
-        },
-        layout: {
-          'line-cap': 'round',
-          'line-join': 'round',
-          'line-z-offset': 10
-        }
-      })
-        */
-    });
-
-    function printMapState() {
-      console.log("Zoom: " + map.getZoom());
-      console.log("Center: " + JSON.stringify(map.getCenter()));
-      console.log("Pitch: " + map.getPitch());
-      console.log("Bearing: " + map.getBearing());
-    }
-
-    map.on('moveend', printMapState);
+    initMap();
   }, []);
 
   return (
---
+++ src/pages/mapbox/utils.ts
@@ -0,0 +1,259 @@
+import mapboxgl from "mapbox-gl";
+import * as d3 from 'd3';
+import * as turf from '@turf/turf';
+
+const createGeoJSONCircle = (center: Array<any>, radiusInKm: number, points = 64) => {
+  const coords = {
+    latitude: center[1],
+    longitude: center[0],
+  };
+  const km = radiusInKm;
+  const ret = [];
+  const distanceX = km / (111.320 * Math.cos((coords.latitude * Math.PI) / 180));
+  const distanceY = km / 110.574;
+  let theta;
+  let x;
+  let y;
+  for (let i = 0; i < points; i += 1) {
+    theta = (i / points) * (2 * Math.PI);
+    x = distanceX * Math.cos(theta);
+    y = distanceY * Math.sin(theta);
+    ret.push([coords.longitude + x, coords.latitude + y]);
+  }
+  ret.push(ret[0]);
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'Polygon',
+      coordinates: [ret],
+    }
+  } as any;
+}
+
+// given a bearing, pitch, altitude, and a targetPosition on the ground to look at,
+// calculate the camera's targetPosition as lngLat
+let previousCameraPosition: { lng: number, lat: number }
+
+// amazingly simple, via https://codepen.io/ma77os/pen/OJPVrP
+function lerp(start: number, end: number, amt: number) {
+  return (1 - amt) * start + amt * end
+}
+
+const computeCameraPosition = (
+  pitch: number,
+  bearing: number,
+  targetPosition: { lng: number, lat: number },
+  altitude: number,
+  smooth = false
+) => {
+  var bearingInRadian = bearing / 57.29;
+  var pitchInRadian = (90 - pitch) / 57.29;
+
+  var lngDiff =
+    ((altitude / Math.tan(pitchInRadian)) *
+      Math.sin(-bearingInRadian)) /
+    70000; // ~70km/degree longitude
+  var latDiff =
+    ((altitude / Math.tan(pitchInRadian)) *
+      Math.cos(-bearingInRadian)) /
+    110000 // 110km/degree latitude
+
+  var correctedLng = targetPosition.lng + lngDiff;
+  var correctedLat = targetPosition.lat - latDiff;
+
+  const newCameraPosition = {
+    lng: correctedLng,
+    lat: correctedLat
+  };
+
+  if (smooth) {
+    if (previousCameraPosition) {
+      const SMOOTH_FACTOR = 0.95
+      newCameraPosition.lng = lerp(newCameraPosition.lng, previousCameraPosition.lng, SMOOTH_FACTOR);
+      newCameraPosition.lat = lerp(newCameraPosition.lat, previousCameraPosition.lat, SMOOTH_FACTOR);
+    }
+  }
+
+  previousCameraPosition = newCameraPosition
+
+  return newCameraPosition
+};
+
+const flyInAndRotate = async ({
+  map,
+  targetLngLat,
+  duration,
+  startAltitude,
+  endAltitude,
+  startBearing,
+  endBearing,
+  startPitch,
+  endPitch,
+}: {
+  map: any;
+  targetLngLat: { lng: number, lat: number },
+  duration: number,
+  startAltitude: number,
+  endAltitude: number,
+  startBearing: number,
+  endBearing: number,
+  startPitch: number,
+  endPitch: number,
+}) => {
+  return new Promise<{
+    bearing: number,
+    altitude: number,
+  }>(async (resolve) => {
+    let start: number;
+
+    var currentAltitude;
+    var currentBearing;
+    var currentPitch;
+
+    // the animation frame will run as many times as necessary until the duration has been reached
+    const frame = async (time: number) => {
+      if (!start) {
+        start = time;
+      }
+
+      // otherwise, use the current time to determine how far along in the duration we are
+      let animationPhase = (time - start) / duration;
+
+      // because the phase calculation is imprecise, the final zoom can vary
+      // if it ended up greater than 1, set it to 1 so that we get the exact endAltitude that was requested
+      if (animationPhase > 1) {
+        animationPhase = 1;
+      }
+
+      currentAltitude = startAltitude + (endAltitude - startAltitude) * d3.easeCubicOut(animationPhase)
+      // rotate the camera between startBearing and endBearing
+      currentBearing = startBearing + (endBearing - startBearing) * d3.easeCubicOut(animationPhase)
+
+      currentPitch = startPitch + (endPitch - startPitch) * d3.easeCubicOut(animationPhase)
+
+      // compute corrected camera ground position, so the start of the path is always in view
+      var correctedPosition = computeCameraPosition(
+        currentPitch,
+        currentBearing,
+        targetLngLat,
+        currentAltitude
+      );
+
+      // set the pitch and bearing of the camera
+      const camera = map.getFreeCameraOptions();
+      camera.setPitchBearing(currentPitch, currentBearing);
+
+      // set the position and altitude of the camera
+      camera.position = mapboxgl.MercatorCoordinate.fromLngLat(
+        correctedPosition,
+        currentAltitude
+      );
+
+      // apply the new camera options
+      map.setFreeCameraOptions(camera);
+
+      // when the animationPhase is done, resolve the promise so the parent function can move on to the next step in the sequence
+      if (animationPhase === 1) {
+        resolve({
+          bearing: currentBearing,
+          altitude: currentAltitude,
+        });
+
+        // return so there are no further iterations of this frame
+        return;
+      }
+
+      await window.requestAnimationFrame(frame);
+    };
+
+    await window.requestAnimationFrame(frame);
+  });
+};
+
+const animatePath = async ({
+  map,
+  duration,
+  path,
+  startBearing,
+  startAltitude,
+  pitch
+}: {
+  map: any;
+  duration: number;
+  path: any;
+  startBearing: number,
+  startAltitude: number,
+  pitch: number,
+}) => {
+  return new Promise<null>(async (resolve) => {
+    const pathDistance = turf.length(path);
+    let startTime: number;
+
+    const frame = async (currentTime: number) => {
+      if (!startTime) startTime = currentTime;
+      const animationPhase = (currentTime - startTime) / duration;
+
+      // when the duration is complete, resolve the promise and stop iterating
+      if (animationPhase > 1) {
+
+        resolve(null);
+        return;
+      }
+
+      // calculate the distance along the path based on the animationPhase
+      const alongPath = turf.along(path, pathDistance * animationPhase).geometry
+        .coordinates;
+
+      const lngLat = {
+        lng: alongPath[0],
+        lat: alongPath[1],
+      };
+
+      // Reduce the visible length of the line by using a line-gradient to cutoff the line
+      // animationPhase is a value between 0 and 1 that reprents the progress of the animation
+      map.setPaintProperty(
+        "line-layer",
+        "line-gradient",
+        [
+          "step",
+          ["line-progress"],
+          "yellow",
+          animationPhase,
+          "rgba(0, 0, 0, 0)",
+        ]
+      );
+
+      // slowly rotate the map at a constant rate
+      const bearing = startBearing - animationPhase * 200.0;
+
+      // compute corrected camera ground position, so that he leading edge of the path is in view
+      var correctedPosition = computeCameraPosition(
+        pitch,
+        bearing,
+        lngLat,
+        startAltitude,
+        true // smooth
+      );
+
+      // set the pitch and bearing of the camera
+      const camera = map.getFreeCameraOptions();
+      camera.setPitchBearing(pitch, bearing);
+
+      // set the position and altitude of the camera
+      camera.position = mapboxgl.MercatorCoordinate.fromLngLat(
+        correctedPosition,
+        startAltitude
+      );
+
+      // apply the new camera options
+      map.setFreeCameraOptions(camera);
+
+      // repeat!
+      await window.requestAnimationFrame(frame);
+    };
+
+    await window.requestAnimationFrame(frame);
+  });
+};
+
+export { createGeoJSONCircle, flyInAndRotate, animatePath };
